442cf684ada729a91365248a97d155cab691d2bd
InGyeong <103246753+igit322@users.noreply.github.com>
2022-08-09 12:17:02 +0900

    Add files via upload

--- js/slides.js
+++ js/slides.js
@@ -19,7 +19,7 @@ function showSlides() {
 
   slides[slideIndex - 1].style.display = "block";
 
-  time = setTimeout(showSlides, 7000);
+  time = setTimeout(showSlides, 3000);
 }
 
 function prevNext(num) {
@@ -40,9 +40,57 @@ function prevNext(num) {
 
   slides[slideIndex - 1].style.display = "block";
 
-  time = setTimeout(showSlides, 7000);
+  time = setTimeout(showSlides, 3000);
 }
 
 function stopSlide() {
   clearTimeout(time);
 }
+
+function playSlide() {
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  if (slideIndex < 1) {
+    slideIndex = slides.length;
+  }
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+
+  time = setTimeout(showSlides, 3000);
+}
+
+// play, stop버튼
+
+var stopBtn = document.querySelector(".stop");
+var playBtn = document.querySelector(".play");
+
+stopBtn.addEventListener("click", function () {
+  stopBtn.style.display = "none";
+  playBtn.style.display = "inline-block";
+});
+
+playBtn.addEventListener("click", function () {
+  playBtn.style.display = "none";
+  stopBtn.style.display = "inline-block";
+});
+
+// prevNext 누를때 일시정지 버튼 바뀜
+
+var prevBtn = document.querySelector(".prev");
+var nextBtn = document.querySelector(".next");
+
+prevBtn.addEventListener("click", function () {
+  playBtn.style.display = "none";
+  stopBtn.style.display = "inline-block";
+});
+
+nextBtn.addEventListener("click", function () {
+  playBtn.style.display = "none";
+  stopBtn.style.display = "inline-block";
+});
